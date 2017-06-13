d4bdb742fd3d7444de763dd7df3801ce5e8ef142
Seth Fitzsimmons <seth@mojodna.net>
2017-06-13 16:14:42 -0700

    Generate prefixed links

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fsExtra = require(`fs-extra`);
 const path = require(`path`);
 const _ = require(`lodash`);
 
-module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
+module.exports = ({ files, linkPrefix, markdownNode, markdownAST, getNode }) => {
   // Copy linked files to the public directory and modify the AST to point to
   // new location of the files.
   const visitor = link => {
@@ -18,7 +18,7 @@ module.exports = ({ files, markdownNode, markdownAST, getNode }) => {
       });
       if (linkNode && linkNode.absolutePath) {
         const newPath = path.join(process.cwd(), `public`, `${linkNode.relativePath}`);
-        link.url = linkNode.relativePath;
+        link.url = path.join(linkPrefix, linkNode.relativePath);
         if (!fsExtra.existsSync(newPath)) {
           fsExtra.copy(linkPath, newPath, err => {
             if (err) {
